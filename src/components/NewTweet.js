65851851cda3b7f1ce8f45e80176d52d96f6c3f2
PhilipDeFraties <philipjfry338@gmail.com>
2023-04-19 14:17:19 -0600

    add NewTweet connected component

---
+++ src/components/NewTweet.js
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import { connect } from "react-redux";
+import { handleAddTweet } from "../actions/tweets";
+
+const NewTweet = ({ dispatch, id }) => {
+  const [text, setText] = useState("");
+
+  const handleChange = (e) => {
+    const text = e.target.value;
+
+    setText(text);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    dispatch(handleAddTweet(text, id))
+
+    setText("");
+  };
+
+  const tweetLeft = 280 - text.length
+
+  return (
+    <div>
+      <h3 className="center">Compose New Tweet</h3>
+      <form className="new-tweet" onSubmit={handleSubmit}>
+        <textarea 
+          placeholder="what's happening?" 
+          value={text} onChange={handleChange} 
+          className="textarea" 
+          maxLength={280}
+        />
+        {tweetLeft <= 100 && <div className="tweet-length">{tweetLeft}</div>}
+        <button className="btn" type="submit" disabled={text === ""}>
+          Submit
+        </button>
+      </form>
+    </div>
+  )
+}
+
+export default connect()(NewTweet);
