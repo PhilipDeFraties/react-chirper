f25b6ceed5e566f4e1a3822ca2b178d5442b0b44
PhilipDeFraties <philipjfry338@gmail.com>
2023-04-19 15:36:21 -0600

    render routes from App's return statement

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,11 @@
-import { useEffect } from "react";
+import { useEffect, Fragment } from "react";
 import { connect } from "react-redux";
 import { handleInitialData } from "../actions/shared";
+import { Routes, Route } from "react-router-dom";
+import Nav from "./Nav";
 import Dashboard from "./Dasboard";
+import NewTweet from "./NewTweet";
+import TweetPage from "./TweetPage";
 import LoadingBar from "react-redux-loading-bar";
 
 const App = (props) => {
@@ -9,10 +13,23 @@ const App = (props) => {
     props.dispatch(handleInitialData())
   }, []);
 
-  return <div>
-    <LoadingBar />
-    {props.loading === true ? null : <Dashboard />}
-  </div>;
+  return  (
+    <Fragment>
+      <LoadingBar />
+      <div className="container">
+        <Nav />
+        {
+          props.loading === true ? null : (
+            <Routes>
+              <Route path="/" exect element={<Dashboard />} />
+              <Route path="/tweet/:id" element={<TweetPage />} />
+              <Route path="/new" element={<NewTweet />} />
+            </Routes>
+          )
+        }
+      </div>
+    </Fragment>
+  )
 };
 
 const mapStateToProps = ({authedUser}) => ({
